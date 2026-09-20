46fb93a0664438bee008a637248aec0524618198
BIKRAM GHOSH <ghoshbikram2725@gmail.com>
2026-09-20 12:14:42 +0530

    Refactor Product schema and restore commented code

--- price-worker/src/models/Product.js
+++ price-worker/src/models/Product.js
@@ -1,5 +1,135 @@
+// import mongoose from "mongoose";
+
+// const priceHistorySchema = new mongoose.Schema(
+//     {
+//         price: {
+//             type: Number,
+//             required: true,
+//             min: 0,
+//         },
+
+//         checkedAt: {
+//             type: Date,
+//             default: Date.now,
+//         },
+//     },
+//     {
+//         _id: false,
+//     }
+// );
+
+// const trackedProductSchema =
+//     new mongoose.Schema(
+//         {
+//             source: {
+//                 type: String,
+//                 required: true,
+//                 enum: [
+//                     "amazon",
+//                     "flipkart",
+//                     "myntra",
+//                 ],
+//             },
+
+//             productKey: {
+//                 type: String,
+//                 required: true,
+//                 trim: true,
+//             },
+
+//             url: {
+//                 type: String,
+//                 required: true,
+//                 trim: true,
+//             },
+
+//             title: {
+//                 type: String,
+//                 required: true,
+//                 trim: true,
+//             },
+
+//             brand: {
+//                 type: String,
+//                 default: "",
+//                 trim: true,
+//             },
+
+//             category: {
+//                 type: String,
+//                 default: "",
+//                 trim: true,
+//             },
+
+//             modelNumber: {
+//                 type: String,
+//                 default: "",
+//                 trim: true,
+//             },
+
+//             image: {
+//                 type: String,
+//                 default: "",
+//             },
+
+//             specifications: {
+//                 type: Map,
+//                 of: String,
+//                 default: {},
+//             },
+
+//             currentPrice: {
+//                 type: Number,
+//                 required: true,
+//                 min: 0,
+//             },
+
+//             mrp: {
+//                 type: Number,
+//                 default: null,
+//                 min: 0,
+//             },
+
+//             inStock: {
+//                 type: Boolean,
+//                 default: true,
+//             },
+
+//             history: {
+//                 type: [priceHistorySchema],
+//                 default: [],
+//             },
+
+//             lastChecked: {
+//                 type: Date,
+//                 default: Date.now,
+//             },
+//         },
+//         {
+//             timestamps: true,
+//         }
+//     );
+
+// trackedProductSchema.index(
+//     {
+//         source: 1,
+//         productKey: 1,
+//     },
+//     {
+//         unique: true,
+//     }
+// );
+
+// export default mongoose.model(
+//     "TrackedProduct",
+//     trackedProductSchema,
+//     "trackedproducts"
+// );
+
+
 import mongoose from "mongoose";
 
+// Price history schema
 const priceHistorySchema = new mongoose.Schema(
     {
         price: {
@@ -18,98 +148,102 @@ const priceHistorySchema = new mongoose.Schema(
     }
 );
 
-const trackedProductSchema =
-    new mongoose.Schema(
-        {
-            source: {
-                type: String,
-                required: true,
-                enum: [
-                    "amazon",
-                    "flipkart",
-                    "myntra",
-                ],
-            },
-
-            productKey: {
-                type: String,
-                required: true,
-                trim: true,
-            },
-
-            url: {
-                type: String,
-                required: true,
-                trim: true,
-            },
-
-            title: {
-                type: String,
-                required: true,
-                trim: true,
-            },
-
-            brand: {
-                type: String,
-                default: "",
-                trim: true,
-            },
-
-            category: {
-                type: String,
-                default: "",
-                trim: true,
-            },
-
-            modelNumber: {
-                type: String,
-                default: "",
-                trim: true,
-            },
-
-            image: {
-                type: String,
-                default: "",
-            },
-
-            specifications: {
-                type: Map,
-                of: String,
-                default: {},
-            },
-
-            currentPrice: {
-                type: Number,
-                required: true,
-                min: 0,
-            },
-
-            mrp: {
-                type: Number,
-                default: null,
-                min: 0,
-            },
-
-            inStock: {
-                type: Boolean,
-                default: true,
-            },
-
-            history: {
-                type: [priceHistorySchema],
-                default: [],
-            },
-
-            lastChecked: {
-                type: Date,
-                default: Date.now,
-            },
+const trackedProductSchema = new mongoose.Schema(
+    {
+        source: {
+            type: String,
+            required: true,
+            enum: [
+                "amazon",
+                "flipkart",
+                "myntra",
+            ],
         },
-        {
-            timestamps: true,
-        }
-    );
 
+        productKey: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        url: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        brand: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+
+        category: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+
+        modelNumber: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+
+        image: {
+            type: String,
+            default: "",
+        },
+
+        specifications: {
+            type: Map,
+            of: String,
+            default: {},
+        },
+
+        // Current selling price
+        currentPrice: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+
+        // Current MRP
+        mrp: {
+            type: Number,
+            default: null,
+            min: 0,
+        },
+
+        inStock: {
+            type: Boolean,
+            default: true,
+        },
+
+        // Price history
+        history: {
+            type: [priceHistorySchema],
+            default: [],
+        },
+
+        // Last time product was checked
+        lastChecked: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Prevent duplicate product on the same platform
 trackedProductSchema.index(
     {
         source: 1,
